fdf02b0aac454a685ef3f7be8d16e49059fd91da
lilinyc <wirelesspremium@gmail.com>
2017-05-10 22:55:06 -0400

    wrote more units test for Calculator class

--- Amazon/src/test/java/unit/Calculator.java
+++ Amazon/src/test/java/unit/Calculator.java
@@ -5,8 +5,33 @@ package unit;
  */
 public class Calculator {
 
-    public int addition(int a, int b){
+    public int addition(int a, int b) {
         int total = a + b;
         return total;
     }
+
+    public int subtraction(int a, int b) {
+        int total = a - b;
+        return total;
+    }
+
+    public int multiplication (int a, int b) {
+        int total = a * b;
+        return total;
+    }
+
+    public double division(double a, double b) {
+        double total = a / b;
+        return total;
+    }
+
+    public double power (double a, double b){
+        double total=Math.pow(a, b);
+        return total;
+    }
+
+
+
 }
+
+
--- Amazon/src/test/java/unit/TestCalculator.java
+++ Amazon/src/test/java/unit/TestCalculator.java
@@ -10,7 +10,36 @@ public class TestCalculator {
     @Test
     public void TestAddition() {
         Calculator calculator = new Calculator();
-        int total = calculator.addition(5,10);
+        int total = calculator.addition(5, 10);
         Assert.assertEquals(total, 15);
     }
+
+    @Test
+    public void TestSubtraction() {
+        Calculator calculator = new Calculator();
+        int total = calculator.subtraction(10, 5);
+        Assert.assertEquals(total, 5);
+    }
+
+    @Test
+    public void TestMultiplication() {
+        Calculator calculator = new Calculator();
+        int total = calculator.multiplication(5, 10);
+        Assert.assertEquals(total, 50);
+    }
+
+    @Test
+    public void TestDivision() {
+        Calculator calculator = new Calculator();
+        double total = calculator.division(10, 2);
+        Assert.assertEquals(total, 5.0, 0);
+    }
+
+    @Test
+    public void TestPower() {
+        Calculator calculator = new Calculator();
+        double total = calculator.power(5, 3);
+        Assert.assertEquals(total, 125.0, 0);
+    }
+
 }
