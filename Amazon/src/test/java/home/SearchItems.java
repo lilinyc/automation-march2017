a887a9263daa8b0c843bc4cd26d49be365466551
lilinyc <wirelesspremium@gmail.com>
2017-05-10 23:26:47 -0400

    write more search test for amazon site

--- Amazon/src/test/java/home/SearchItems.java
+++ Amazon/src/test/java/home/SearchItems.java
@@ -3,6 +3,7 @@ package home;
 import base.CommonAPI;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
@@ -11,8 +12,26 @@ import org.testng.annotations.Test;
 public class SearchItems extends CommonAPI {
 
     @Test
-    public void serach(){
+    public void TestSerachBasic(){
         typeByCss("#twotabsearchtextbox", "iphone");
         clickByCss(".nav-input");
+    }
+
+    @Test
+    public void TestSerachCategories(){
+        typeByCss("#twotabsearchtextbox", "Lenovo Yoga 14");
+        clickByCss(".nav-input");
+        waitUntilVisible(By.cssSelector(".categoryRefinementsSection"));
+        String category = getTextByCss(".categoryRefinementsSection ul li a span:first-child");
+        Assert.assertEquals(category, "Computers & Accessories");
+    }
+
+    @Test
+    public void TestSearchLaptop(){
+        typeByCss("#twotabsearchtextbox", "Lenovo Yoga 710 80V4000GUS");
+        clickByCss(".nav-input");
+        waitUntilVisible(By.cssSelector(".categoryRefinementsSection"));
+        String laptopName = getTextByCss(".s-access-title");
+        Assert.assertEquals(laptopName.contains("Lenovo Yoga 710 80V4000GUS"), true);
     }
 }
