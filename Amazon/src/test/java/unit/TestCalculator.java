5edabe729bf54a1adb29f6dcd4d461d81f2cd543
lilinyc <wirelesspremium@gmail.com>
2017-05-10 22:19:36 -0400

    added git ignore file and updated test calculator test

--- Amazon/src/test/java/unit/TestCalculator.java
+++ Amazon/src/test/java/unit/TestCalculator.java
@@ -1,12 +1,14 @@
 package unit;
 
 import org.junit.Assert;
+import org.testng.annotations.Test;
 
 /**
  * Created by PIIT_NYA on 5/6/2017.
  */
 public class TestCalculator {
-    public static void main(String[] args) {
+    @Test
+    public void TestAddition() {
         Calculator calculator = new Calculator();
         int total = calculator.addition(5,10);
         Assert.assertEquals(total, 15);
