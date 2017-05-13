47dade07e91ddcd5ccfb9c6b5153e23942f3ad75
lilinyc <wirelesspremium@gmail.com>
2017-05-12 20:28:26 -0400

    wrote adding item to cart test for different product products added: mouse, headphones, laptop

--- Amazon/src/test/java/home/SearchItems.java
+++ Amazon/src/test/java/home/SearchItems.java
@@ -31,7 +31,107 @@ public class SearchItems extends CommonAPI {
         typeByCss("#twotabsearchtextbox", "Lenovo Yoga 710 80V4000GUS");
         clickByCss(".nav-input");
         waitUntilVisible(By.cssSelector(".categoryRefinementsSection"));
-        String laptopName = getTextByCss(".s-access-title");
-        Assert.assertEquals(laptopName.contains("Lenovo Yoga 710 80V4000GUS"), true);
+        String productName = getTextByCss(".s-access-title");
+        Assert.assertEquals(productName.contains("Lenovo Yoga 710 80V4000GUS"), true);
     }
+
+    @Test
+    public void TestSearchLaptopDrillDown(){
+        typeByCss("#twotabsearchtextbox", "Lenovo Yoga 710 80V4000GUS");
+        clickByCss(".nav-input");
+        waitUntilVisible(By.cssSelector(".categoryRefinementsSection"));
+        clickByCss(".s-access-title");
+        waitUntilVisible(By.cssSelector("#productTitle"));
+        String productName = getTextByCss("#productTitle");
+        Assert.assertEquals(productName.contains("Lenovo Yoga 710 80V4000GUS"), true);
+    }
+
+    @Test
+    public void  TestSearchLaptopOrder(){
+        typeByCss("#twotabsearchtextbox", "Lenovo Yoga 710 80V4000GUS");
+        clickByCss(".nav-input");
+        waitUntilVisible(By.cssSelector(".categoryRefinementsSection"));
+        clickByCss(".s-access-title");
+        waitUntilVisible(By.cssSelector("#productTitle"));
+        clickByCss("#add-to-cart-button");
+
+        waitUntilVisible(By.cssSelector("#huc-v2-order-row-confirm-text"));
+        String addToCartText = getTextByCss("#huc-v2-order-row-confirm-text");
+        Assert.assertEquals(addToCartText.contains("Added to Cart"), true);
+        String productName = getTextByCss(".huc-v2-upsell-title");
+        Assert.assertEquals(productName.contains("Lenovo Yoga 710 80V4000GUS"), true);
+    }
+
+    @Test
+    public void TestSearchHeadphones(){
+        typeByCss("#twotabsearchtextbox", "headphones seinnheiser 360");
+        clickByCss(".nav-input");
+        waitUntilVisible(By.cssSelector(".categoryRefinementsSection"));
+        String productName = getTextByCss(".s-access-title");
+        Assert.assertEquals(productName.contains("Sennheiser PC 360 Special Edition"), true);
+    }
+
+    @Test
+    public void TestSearchHeadphonesDrillDown(){
+        typeByCss("#twotabsearchtextbox", "headphones seinnheiser 360");
+        clickByCss(".nav-input");
+        waitUntilVisible(By.cssSelector(".categoryRefinementsSection"));
+        clickByCss(".s-access-title");
+        waitUntilVisible(By.cssSelector("#productTitle"));
+        String productName = getTextByCss("#productTitle");
+        Assert.assertEquals(productName.contains("Sennheiser PC 360 Special Edition"), true);
+    }
+
+    @Test
+    public void  TestSearchHeadphonesOrder(){
+        typeByCss("#twotabsearchtextbox", "headphones seinnheiser 360");
+        clickByCss(".nav-input");
+        waitUntilVisible(By.cssSelector(".categoryRefinementsSection"));
+        clickByCss(".s-access-title");
+        waitUntilVisible(By.cssSelector("#productTitle"));
+        clickByCss("#add-to-cart-button");
+
+        waitUntilVisible(By.cssSelector("#huc-v2-order-row-confirm-text"));
+        String addToCartText = getTextByCss("#huc-v2-order-row-confirm-text");
+        Assert.assertEquals(addToCartText.contains("Added to Cart"), true);
+        String productName = getTextByCss(".huc-v2-upsell-title");
+        Assert.assertEquals(productName.contains("Sennheiser PC 360 Special Edition"), true);
+    }
+
+    @Test
+    public void TestSearchMouse(){
+        typeByCss("#twotabsearchtextbox", "mouse logitech g502");
+        clickByCss(".nav-input");
+        waitUntilVisible(By.cssSelector(".categoryRefinementsSection"));
+        String productName = getTextByCss(".s-access-title");
+        Assert.assertEquals(productName.contains("Logitech G502 Proteus"), true);
+    }
+
+    @Test
+    public void TestSearchMouseDrillDown(){
+        typeByCss("#twotabsearchtextbox", "mouse logitech g502");
+        clickByCss(".nav-input");
+        waitUntilVisible(By.cssSelector(".categoryRefinementsSection"));
+        clickByCss(".s-access-title");
+        waitUntilVisible(By.cssSelector("#productTitle"));
+        String productName = getTextByCss("#productTitle");
+        Assert.assertEquals(productName.contains("Logitech G502 Proteus"), true);
+    }
+
+    @Test
+    public void TestSearchMouseOrder(){
+        typeByCss("#twotabsearchtextbox", "mouse logitech g502");
+        clickByCss(".nav-input");
+        waitUntilVisible(By.cssSelector(".categoryRefinementsSection"));
+        clickByCss(".s-access-title");
+        waitUntilVisible(By.cssSelector("#productTitle"));
+        clickByCss("#add-to-cart-button");
+
+        waitUntilVisible(By.cssSelector("#huc-v2-order-row-confirm-text"));
+        String addToCartText = getTextByCss("#huc-v2-order-row-confirm-text");
+        Assert.assertEquals(addToCartText.contains("Added to Cart"), true);
+        String productName = getTextByCss(".huc-v2-upsell-title");
+        Assert.assertEquals(productName.contains("Logitech G502 Proteus"), true);
+    }
+
 }
--- Amazon/src/test/java/home/TestAmazonHome.java
+++ Amazon/src/test/java/home/TestAmazonHome.java
@@ -1,9 +1,9 @@
 package home;
 
-import base.CommonAPI;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-import org.openqa.selenium.By;
+        import base.CommonAPI;
+        import org.testng.Assert;
+        import org.testng.annotations.Test;
+        import org.openqa.selenium.By;
 
 /**
  * Created by PIIT_NYA on 5/6/2017.
